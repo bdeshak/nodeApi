369d3ee241bf8607f162cb8c8130a6ec4c4fc694
Eshak <43699326+bdeshak@users.noreply.github.com>
2024-03-22 20:47:31 +0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import path from "path";
 import * as fs from "fs";
 import cron from "node-cron";
 import ReseedAction from "./mongo/ReseedAction.js";
-const mongoose = require('mongoose');
+import mongoose from 'mongoose';
 
 // MongoDB Atlas connection URI
 const uri = 'mongodb+srv://bdeshak:Mlpnkobj@cluster0.cvl8ljb.mongodb.net/?retryWrites=true&w=majority';
